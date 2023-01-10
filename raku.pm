26d0e0b80b3813c4087785317997328c56372985
Raku Cyber <121725186+raku8200@users.noreply.github.com>
2023-01-10 12:40:32 +0300

    Update raku.pm

--- raku.pm
+++ raku.pm
@@ -1,3 +1,4 @@
+#1/10/2023
 #!/usr/bin/perl
 
 use strict;
@@ -5,22 +6,47 @@ use warnings;
 use LWP::UserAgent;
 use HTTP::Headers;
 use Socket;
+use Term::ANSIColor;
+use 5.010;
+my $datetime    = localtime;
 
+if ($^O =~ /MSWin32/) {system("cls"); }else { system("clear"); }
+print color('bold green');
+print q(
+      ⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⡿⠋⢹⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀
+⠀⠀⠀⣴⣿⠟⣿⣿⣿⣿⣿⣿⣿⠟⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⠻⣿⣦⠀⠀⠀
+⠀⢠⣾⡿⠋⣼⣿⣿⣿⣿⣿⣿⡏⠀⠀⠘⠻⠿⢿⣿⣿⣿⣿⣿⣧⠙⢿⣷⡄⠀
+⢰⣿⡟⠀⠀⣿⣿⣧⡈⠉⠛⠿⠁⢠⣶⣶⡀⠀⠀⠈⠻⢿⣿⣿⣿⠀⠀⢻⣿⡆
+⠈⢿⣿⣄⠀⣿⣿⣿⣿⣦⣄⡀⠀⠈⠛⠛⠀⢸⣷⣶⣤⣈⣿⣿⣿⠀⣠⣿⡿⠁
+⠀⠀⠻⣿⣧⡸⣿⣿⣿⣿⣿⣿⣿⣷⡆⠀⢀⣿⣿⣿⣿⣿⣿⣿⢇⣴⣿⠟⠀⠀
+⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣿⣿⣧⣶⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠿⠿⠿⠿⠿⠿⠟⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+);
+
+print color('reset');
+print "                       ";
+print colored ("[ R Raku v2.1 ]",'white on_red');
+print colored ("[ Coded By Raku 8200 ]\n",'white on_red');
+print "                           ";
+print colored ("[ Start At $datetime ]",'white on_red'),"\n\n";
 
 my $ua = LWP::UserAgent->new;
 
-print "IP Address". "\t". "Domain". "\t". "Url". "\t". "Status Code" . "\n";
+print "IP Address:". "\t". "\tDomain:". "\t"."\t". "\tUrl:". "\t"."\t" ."\t". "\Status Code:" . "\n";
 
 while (my $domain = <STDIN>) {
     chomp $domain;
-    
+
     my $url = "https://$domain";
 
-    
+
     my $response = $ua->get($url);
 
-    
+
     my $ip = inet_ntoa(inet_aton($domain));
-    
-    print $ip . "\t" . $domain . "\t" . $url . "\t" . $response->code . "\n";
+
+    print $ip . "\t" ."\t". $domain . "\t" . $url . "\t" ."\t" ."\t". $response->code . "\n";
 }
